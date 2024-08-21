db5533e6dd8e341253e4aabfdf37d5c168b26ad9
ItsRiprod <riprodgames@gmail.com>
2024-08-21 16:16:57 -0400

    v0.8.1

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -14,8 +14,8 @@ const App: React.FC = () => {
     })
 
     return (
-        <div className="bg-slate-800 justify-center items-center">
-            <p className="font-bold text-xl">DeskThing App</p>
+        <div className="bg-slate-800 w-screen h-screen flex justify-center items-center">
+            <p className="font-bold text-5xl text-white">DeskThing App</p>
         </div>
 
     )
